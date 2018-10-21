b15280f1f2d62e80b107a6bcf9c0d7e4de52c25b
Kael <artmichael08@gmail.com>
2018-10-21 22:42:29 +0800

    update users client table

--- resources/views/admin-console-users.blade.php
+++ resources/views/admin-console-users.blade.php
@@ -50,14 +50,14 @@
               <table class="table tbl-client-users" cellspacing="0" width="100%">
                 <thead>
                   <tr>
-                    <th></th><th>Curacall ID</th>
+                    <th></th><th>Curacall ID</th><th>Role</th><th>First name</th><th> Last name</th><th>Email</th><th>Account</th><th>Status</th>
                   </tr>
                 </thead>
                 <tbody>
                 </tbody>
                  <tfoot>
                   <tr>
-                    <th></th><th>Curacall ID</th>
+                    <th></th><th>Curacall ID</th><th>Role</th><th>First name</th><th> Last name</th><th>Email</th><th>Account</th><th>Status</th>
                   </tr>
                 </tfoot>
               </table>
@@ -138,7 +138,13 @@
       ajax: "{{ url('admin/client-users') }}",
       columns: [
         {data: 'img', orderable: false, searchable: false},
-        {data: 'curacall_id', name: 'users.id'}
+        {data: 'curacall_id', name: 'users.id'},
+        {data: 'role_title', name : 'c.role_title'}, 
+        {data: 'fname', name : 'users.fname'}, 
+        {data: 'lname', name : 'users.lname'},
+        {data: 'email',name : 'users.email'},
+        {data: 'account_name',name : 'b.account_name'},
+        {data: 'status',name: 'users.status'}
       ]
     });
 
